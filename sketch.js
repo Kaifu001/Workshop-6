28b98857cdc7df64f5ab2112b6b798d12fe61b08
Kaifu001 <kaifu.yang@icloud.com>
2025-01-30 16:56:17 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,49 @@
+let originalPoem = [
+  "O me O life",
+  "O Captain My Captian"
+];
+
+let inputField, button, outputText = "";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
+  textSize(16);
+
+  inputField = createInput("O"); 
+  inputField.position(20, 50);
+
+  button = createButton("Find");
+  button.position(180, 50);
+  button.mousePressed(generatePoem);
+}
+
+function generatePoem() {
+  let userWord = inputField.value(); 
+  let newPoem = [];
+
+  for (let line of originalPoem) {
+    let words = RiTa.tokenize(line); 
+    let newLine = [];
+
+    for (let word of words) {
+      if (word === userWord) {
+        let rhymes = RiTa.rhymes(word); 
+        let synonyms = RiTa.similarBySound(word);
+        let randomWord = random([...rhymes, ...synonyms, "captian", "me", "life"]); 
+        newLine.push(randomWord || word);
+      } else {
+        newLine.push(word);
+      }
+    }
+
+    newPoem.push(newLine.join(""));
+  }
+
+  outputText = newPoem.join("\n");
 }
 
 function draw() {
-  background(220);
+  background(240);
+  text("Search a key word：", 20, 30);
+  text(outputText, 20, 120, 460, 300); 
 }
